3152248231643cff0ca5c06761bc166317f7838e
Loi Van Tran <datloiboi@gmail.com>
2020-10-08 15:57:31 +0700

    Test config again

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello World hahaha! Sksksksksksk</div>
+  return <div>Hello World Thisi is my first project~!~!~!</div>
 }
